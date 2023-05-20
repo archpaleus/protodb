f27ad6bd7a1ff5268186da3d996e52aec7bdbd0f
archpaleus <archpaleus@gmail.com>
2023-05-20 00:06:47 -0700

    moar

--- src/protobunny/inspectproto/main.cc
+++ src/protobunny/inspectproto/main.cc
@@ -1,13 +1,17 @@
 #include "absl/log/initialize.h"
 #include "absl/log/log.h"
+#include "protobunny/inspectproto/command_line_parser.h"
 
 namespace protobunny::inspectproto {
 
 int Main(int argc, char* argv[]) {
   absl::InitializeLog();
 
-  // CommandLineInterface cli;
-  // return cli.Run(argc, argv);
+  CommandLineParser parser;
+  auto maybe_args = parser.ParseArguments(argc, argv);
+  if (maybe_args) {
+    return -1;
+  }
 
   return 0;
 }
